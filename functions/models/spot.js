03cd0fd1bd1e1ead8421be4159323f3399587df5
junyoii <zxzimin@naver.com>
2022-01-15 00:21:14 +0900

    [ADD] Spot 클래스 작성 (#6)

---
+++ functions/models/spot.js
@@ -0,0 +1,59 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../loaders/sequelize');
+
+const spot = sequelize.define('spot', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    address: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    latitude: {
+        type: DataTypes.DOUBLE,
+        allowNull: false
+    },
+    longitude: {
+        type: DataTypes.DOUBLE,
+        allowNull: false
+    },
+    day: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    sequence: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    next_spot_name: {
+        type: DataTypes.STRING,
+    },
+    next_spot_required_time: {
+        type: DataTypes.STRING,
+    },
+    next_spot_mobility: {
+        type: DataTypes.STRING,
+    },
+}, {
+    sequelize,
+    modelName: 'spot',
+    freezeTableName: true,
+    underscored: true,
+    charset: 'utf8',
+    collate: "utf8_general_ci",
+    timestamps: true,
+    paranoid: true,
+    deletedAt: 'is_deleted'
+});
+
+module.exports = spot;
