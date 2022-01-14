56df270933b4add65952817809c0c07967ed151f
LeeJejune <leejejune0625@gmail.com>
2022-01-15 02:38:50 +0900

    [FEAT] 최신 여행 일정 조회 구현 (#5)

--- functions/controller/postController.js
+++ functions/controller/postController.js
@@ -4,16 +4,24 @@ const responseMessage = require('../constants/responseMessage');
 const { postService } = require('../service');
 
 const retrievePopularPosts = async (req, res) => {
-    try {
-        return res.status(statusCode.OK)
-            .json(util.success(await postService.retrievePopularPosts()));
-    } catch (e) {
-        console.log(e);
-        return res.status(statusCode.BAD_REQUEST)
-            .json(util.fail(statusCode.BAD_REQUEST, responseMessage.VALIDATION_EXCEPTION));
-    }
-}
+  try {
+    return res.status(statusCode.OK).json(util.success(await postService.retrievePopularPosts()));
+  } catch (e) {
+    console.log(e);
+    return res.status(statusCode.BAD_REQUEST).json(util.fail(statusCode.BAD_REQUEST, responseMessage.VALIDATION_EXCEPTION));
+  }
+};
+
+const retrieveLatestPosts = async (req, res) => {
+  try {
+    return res.status(statusCode.OK).json(util.success(await postService.retrieveLatestPosts()));
+  } catch (e) {
+    console.log(e);
+    return res.status(statusCode.BAD_REQUEST).json(util.fail(statusCode.BAD_REQUEST, responseMessage.VALIDATION_EXCEPTION));
+  }
+};
 
 module.exports = {
-    retrievePopularPosts,
+  retrievePopularPosts,
+  retrieveLatestPosts,
 };
