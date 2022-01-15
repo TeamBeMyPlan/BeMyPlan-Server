beeb99d99e386cea6d40f2cb39a8bc0f67f95114
Junyoung Ahn <zxzimin@naver.com>
2022-01-15 15:17:15 +0900

    Revert "[#5] 최신 여행 후기 조회 구현 "

--- functions/api/routes/postApi.js
+++ functions/api/routes/postApi.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-const { postController } = require('../../controller');
+const { postController } = require('../../controller')
 
 router.get('/popular', postController.retrievePopularPosts);
-router.get('/new', postController.retrieveLatestPosts);
 
 module.exports = router;
