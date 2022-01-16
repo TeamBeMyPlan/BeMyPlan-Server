f3ef89b3ee86689b82a445e7efb193841f49fa56
LeeJejune <leejejune0625@gmail.com>
2022-01-16 19:15:07 +0900

    [#22] 구매한 컨텐츠 조회 기능

--- functions/api/routes/index.js
+++ functions/api/routes/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const postApi = require('./postApi');
 const areaApi = require('./areaApi');
+const orderApi = require('./orderApi');
 
 router.use('/post', postApi);
 router.use('/area', areaApi);
+router.use('/order', orderApi);
 module.exports = router;
--- functions/service/index.js
+++ functions/service/index.js
@@ -1,6 +1,8 @@
 const postService = require('./postService');
 const areaService = require('./areaService');
+const orderService = require('./orderService');
 module.exports = {
   postService,
   areaService,
+  orderService
 };
