70f0e8523b95cb5410320f2204c7a931d2998b21
LeeJejune <leejejune0625@gmail.com>
2022-01-18 01:46:51 +0900

    [#35] 중복 API 라우터 제거

--- functions/api/routes/postApi.js
+++ functions/api/routes/postApi.js
@@ -4,16 +4,10 @@ const router = express.Router();
 const { postController } = require('../../controller')
 
 router.get('/popular', postController.retrievePopularPosts);
-
 router.get('/new', postController.retrieveLatestPosts);
-router.get('/new/list', postController.retrieveLatestListPosts);
-
 router.get('/suggest', postController.retrieveRecommendationPosts);
-router.get('/suggest/list', postController.retrieveRecommendationListPosts);
-
 router.get('/:postId/preview', postController.retrievePreviews);
 router.get('/:postId/preview/tag', postController.retrievePreviewTags);
-
 router.get('/', postController.retrieveAuthorPosts);
 
 module.exports = router;
