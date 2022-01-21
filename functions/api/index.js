bc74484b6e545bc317e715dc1cadcd2989d430f8
KimDoubleB <tree9295@gmail.com>
2022-01-21 17:22:39 +0900

    feat: added slack signup alarm

--- functions/api/index.js
+++ functions/api/index.js
@@ -18,20 +18,20 @@ const app = express();
 app.use(cors());
 
 sentry.init({
-  dsn: process.env.SENTRY_DSN,
-  integrations: [
-    new sentry.Integrations.Http({tracing: true}),
-    new tracing.Integrations.Express({app}),
-  ],
-  tracesSampleRate: 1.0,
+    dsn: process.env.SENTRY_DSN,
+    integrations: [
+        new sentry.Integrations.Http({tracing: true}),
+        new tracing.Integrations.Express({app}),
+    ],
+    tracesSampleRate: 1.0,
 });
 
 app.use(sentry.Handlers.requestHandler());
 app.use(sentry.Handlers.tracingHandler());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(hpp());
-  app.use(helmet());
+    app.use(hpp());
+    app.use(helmet());
 }
 
 app.use(express.json());
@@ -44,27 +44,28 @@ app.use(sentry.Handlers.errorHandler());
 
 app.use(function onError(err, req, res, next) {
     slackBot.send('api-서버-로그',
-        `오류가 발생했습니다 !\n\`Error Message\`: ${err.message}\n`).then();
+        `오류가 발생했습니다 !\n
+        \`Error Message\`: ${err.message}\n`).then();
 
     res.status(statusCode.BAD_REQUEST)
       .json(util.fail(statusCode.BAD_REQUEST, err.message))
 });
 
 app.use("*", (req, res) => {
-  res.status(404).json({
-    statusCode: 404,
-    message: "존재하지 않는 경로입니다.",
-  });
+    res.status(404).json({
+        statusCode: 404,
+        message: "존재하지 않는 경로입니다.",
+    });
 });
 
 module.exports = functions
     .runWith({
-      timeoutSeconds: 300,
-      memory: "512MB",
+        timeoutSeconds: 300,
+        memory: "512MB",
     })
     .region("asia-northeast3")
     .https.onRequest(async (req, res) => {
-      console.log("\n\n", "[api]", `[${req.method.toUpperCase()}]`, req.originalUrl, req.body);
+        console.log("\n\n", "[api]", `[${req.method.toUpperCase()}]`, req.originalUrl, req.body);
 
-      return app(req, res);
+        return app(req, res);
     });
